654967af0fb8b6eaebc24d543104b59c1e302d2a
Walt Williams <amboufaye1@gmail.com>
2022-01-29 21:15:18 -0600

    completed files

---
+++ create_map.py
@@ -0,0 +1,47 @@
+from gradcam import GradCAM
+from tensorflow.keras.applications import ResNet50, VGG16
+from tensorflow.keras.preprocessing.image import img_to_array, load_img
+from tensorflow.keras.applications import imagenet_utils
+import numpy as np
+import argparse
+import imutils
+import cv2 as cv
+
+# Create argument parser and parse arguments
+ap = argparse.ArgumentParser()
+ap.add_argument('-i', '--image', required=True, help='path to input image')
+ap.add_argument('-m', '--model', required=True, type=str, default='vgg', 
+choices=('vgg', 'resnet'), help='path to model')
+args = vars(ap.parse_args())
+
+Model = VGG16 if args['model']=='vgg' else ResNet50
+print('loading model...')
+model = Model(weights='imagenet')
+
+# Preprocess image
+orig = cv.imread(args['image'])
+resized = cv.resize(orig, (224,224))
+img = load_img(args['image'], target_size=(224,224))
+img = img_to_array(img)
+img = np.expand_dims(img, axis=0)
+img = imagenet_utils.preprocess_input(img)
+
+# Get predictions
+preds = model.predict(img)
+i = np.argmax(preds[0])
+decoded = imagenet_utils.decode_predictions(preds)
+(imagenetID, label, prob) = decoded[0][0]
+print(f'{label}: {prob*100:.2f}')
+
+# Initialize grad cam and build heatmap
+cam = GradCAM(model, i)
+heatmap = cam.computeHeatmap(img)
+heatmap = cv.resize(heatmap, (orig.shape[1], orig.shape[0]))
+(heatmap, output) = cam.overlayHeatmap(heatmap, orig, alpha=0.5)
+# draw label on output
+cv.rectangle(output, (0,0), (340,40), (0,0,0), -1)
+cv.putText(output, label, (10, 25), cv.FONT_HERSHEY_SIMPLEX, 0.8, (255,255,255), 2)
+output = np.vstack([orig, heatmap, output])
+output = imutils.resize(output, height=700)
+cv.imshow('output', output)
+cv.waitKey(0)
---
+++ gradcam.py
@@ -0,0 +1,62 @@
+from mimetypes import init
+from pyexpat import model
+from tensorflow.keras.models import Model
+import tensorflow as tf
+import numpy as np 
+import cv2 as cv
+import imutils
+
+class GradCAM:
+    
+    def __init__(self, model, classIdx, layerName=None) -> None:
+        self.model = model
+        self.classIdx = classIdx
+        self.layerName = layerName
+
+        if self.layerName is None:
+            self.layerName = self.find_target_layer()
+        
+    def find_target_layer(self):
+        """Designed to find final conv+pooling layer in the CNN to produce class activation map"""
+        for layer in reversed(self.model.layers):
+            if len(layer.output_shape)==4:
+                return layer.name
+            else: 
+                raise ValueError("Could not find layer with 4D output")
+
+    def computeHeatmap(self, image, eps=1e-8):
+        gradModel = Model(
+            inputs=[self.model.inputs],
+            outputs=[self.model.get_layer(self.layerName).output,
+            self.model.output]
+        )
+
+        with tf.GradientTape() as tape:
+            inputs = tf.cast(image, tf.float32)
+            convOutputs, predictions = gradModel(inputs)
+            loss = predictions[:, self.classIdx]
+
+        grads = tape.gradient(loss, convOutputs)
+        castConvOutputs = tf.cast(convOutputs>0, 'float32')
+        castGrads = tf.cast(grads>0, 'float32')
+        guidedGrads = castConvOutputs * castGrads * grads
+
+        # Compute average of gradient values to use as weights
+        weights = tf.reduce_mean(guidedGrads, axis=(0,1))
+        cam = tf.reduce_sum(tf.multiply(weights, convOutputs), axis=-1)
+
+        # Generate heatmap
+        (w,h) = (image.shape[2], image.shape[1])
+        heatmap = cv.resize(cam.numpy(), (w,h))
+        
+        # min-max scaling
+        numer = heatmap - np.min(heatmap)
+        denom = (heatmap.max() - heatmap.min()) + eps
+        heatmap = numer / denom
+        heatmap = (heatmap*255).astype('uint8')
+        return heatmap
+        
+    def overlayHeatmap(self, heatmap, image, alpha=0.5, colormap=cv.COLORMAP_VIRIDIS):
+        heatmap = cv.applyColorMap(heatmap, colormap)
+        output = cv.addWeighted(image, alpha, heatmap, 1-alpha, 0)
+        return (heatmap, output)
